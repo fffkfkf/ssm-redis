4be7951b3023ce76553d491a745e0d56087e9fa0
fffkfkf <gmqhpu@163.com>
2019-08-16 16:19:28 +0800

    成功增加了定时任务,quarts.

---
+++ src/main/java/com/ssm/task/Job1Demo.java
@@ -0,0 +1,15 @@
+package com.ssm.task;
+
+import java.util.Date;
+
+/**
+ * @author gmq
+ * @date 2019/8/16
+ * 版权：Copyright 2000-2001 si-tech.com.cn  All Rights Reserved.
+ */
+public class Job1Demo {
+    public void sayHello1() {
+        System.out.println(new Date() + " -> Hello, 我是任务 1");
+    }
+
+}
