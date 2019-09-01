22325b70cd080b70f9420d6deae3f3760df1afcc
fffkfkf <gmqhpu@163.com>
2019-09-01 14:04:40 +0800

    2,

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ssm传统的demo,引入redis
 JavaEE开发的颠覆者SpringBoot实战
 
+##1
+有这个的拦截
+<url-pattern>*.action</url-pattern>
+所以请求要加上.action, 例如//http://localhost:8081/user.action
--- src/main/java/com/ssm/controller/UserController.java
+++ src/main/java/com/ssm/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.ssm.controller;
 
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ssm.pojo.User;
 import com.ssm.service.UserService;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -16,13 +20,14 @@ import javax.servlet.http.HttpServletRequest;
 public class UserController {
     @Autowired
     public UserService userService;
-
+    //http://localhost:8081/user.action
     @RequestMapping(value = "/user")
     public String getUserById(Model model, HttpServletRequest request) {
         User user = userService.selectByPrimaryKey(1);
         System.out.println(user + "----------------------------------------" + user.getPname());
         model.addAttribute("cc", user);
         request.getSession().setAttribute("u", user);
-        return "main";
+        return "main.jsp";
     }
+
 }
